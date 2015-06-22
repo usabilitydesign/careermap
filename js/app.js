bfc1363121c20e467f41909f82929edcb69cd453
Leigh Frampton <leigh765@me.com>
2015-06-22 14:47:52 -0400

    init

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+var app = angular.module('finderscope',
+  [ 'finderscope.config'
+  , 'finderscope.controllers.dataController'
+  ,'iso.directives'
+  
+  
+  ])
+	
+
+
---
+++ js/controllers/dataController.js
@@ -0,0 +1,251 @@
+'use strict';
+angular.module('finderscope.controllers.dataController', [] ).
+	 controller("resumeController", ["$scope",function($scope) {
+	     	
+		    	Tabletop.init( { key: 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=1guTLsqqQf2xMejYZ3Mf2p875EYbgxHKSfCEOI90GPDc&output=html',
+	            callback: showInfo,
+	            simpleSheet: true } );
+	  			
+	  			var data_array = [];
+
+			  	function showInfo(data, tabletop) {
+			  		
+			    	//alert("Successfully processed!")
+			    	console.log("data");
+			    	
+			    	angular.forEach(data, function(value, key){
+			    		this.push(key + ":" + value);
+			    	}, data_array);
+			    	// console.log(data_array)
+			    	return data_array;
+			    	
+
+			  	}
+			  	
+			  $scope.lily = [100,79,31,137,3,21,16,29,30,12,3,45,3,61];
+			  $scope.xList =[
+    {
+        "name": "Casual Dining",
+        "class": "one a",
+        "sub": "service",
+        "years": "3",
+        "dateend": "1213867342000",
+        "video": "false",
+        "rec": "0",
+        "high_salary": "7",
+        "tasks": "register,\nhostess,\ntable service"
+    },
+    {
+        "name": "Casual Dining",
+        "class": "one b",
+        "sub": "prep",
+        "years": "1",
+        "dateend": "1213867342000",
+        "video": "false",
+        "rec": "0",
+        "high_salary": "6",
+        "tasks": "baker,\nfryer,\noven,\ndeli,\nplating"
+    },
+    {
+        "name": "Casual Dining",
+        "class": "one c",
+        "sub": "maintenance",
+        "years": "2",
+        "dateend": "1213867342000",
+        "video": "false",
+        "rec": "0",
+        "high_salary": "7.5",
+        "tasks": "dining room cleaning,\nkitchen cleaning,\nextermination prep,\nsetup"
+    },
+    {
+        "name": "Casual Dining",
+        "class": "one d",
+        "sub": "manage",
+        "years": "3",
+        "dateend": "1213867342000",
+        "video": "false",
+        "rec": "1",
+        "high_salary": "7",
+        "tasks": "close out register"
+    },
+    {
+        "name": "Casual Dining",
+        "class": "one e",
+        "sub": "cert",
+        "years": "1",
+        "dateend": "1213867342000",
+        "video": "false",
+        "rec": "1",
+        "high_salary": "7",
+        "tasks": "ABC Manager"
+    },
+    {
+        "name": "Fine Dining",
+        "class": "two a",
+        "sub": "service",
+        "years": "5",
+        "dateend": "1276939342000",
+        "video": "false",
+        "rec": "1",
+        "high_salary": "20",
+        "tasks": "hostess,\ntable service,\nbartending"
+    },
+    {
+        "name": "Fine Dining",
+        "class": "two b",
+        "sub": "prep",
+        "years": "5",
+        "dateend": "1276939342000",
+        "video": "false",
+        "rec": "2",
+        "high_salary": "20",
+        "tasks": ""
+    },
+    {
+        "name": "Fine Dining",
+        "class": "two c",
+        "sub": "maintenance",
+        "years": "3",
+        "dateend": "1276939342000",
+        "video": "false",
+        "rec": "9",
+        "high_salary": "20",
+        "tasks": ""
+    },
+    {
+        "name": "Fine Dining",
+        "class": "two d",
+        "sub": "manage",
+        "years": "3",
+        "dateend": "1276939342000",
+        "video": "false",
+        "rec": "4",
+        "high_salary": "20",
+        "tasks": ""
+    },
+    {
+        "name": "Fine Dining",
+        "class": "two e",
+        "sub": "cert",
+        "years": "3",
+        "dateend": "1276939342000",
+        "video": "false",
+        "rec": "4",
+        "high_salary": "20",
+        "tasks": "ABC Manager,\nFood Handling Certification"
+    },
+    {
+        "name": "Catering",
+        "class": "three a",
+        "sub": "service",
+        "years": "9",
+        "dateend": "1403169742000",
+        "video": "true",
+        "rec": "2",
+        "high_salary": "45",
+        "tasks": "table service,\nbartending,\nfrench table service,\nhorderve service,\nbuffet service"
+    },
+    {
+        "name": "Catering",
+        "class": "three b",
+        "sub": "prep",
+        "years": "9",
+        "dateend": "1403169742000",
+        "video": "true",
+        "rec": "3",
+        "high_salary": "45",
+        "tasks": "baker,\nfryer,\noven,\ndeli,\nhorderve,\nplating"
+    },
+    {
+        "name": "Catering",
+        "class": "three c",
+        "sub": "maintenance",
+        "years": "7",
+        "dateend": "1403169742000",
+        "video": "true",
+        "rec": "5",
+        "high_salary": "45",
+        "tasks": "setup,\nbreakdown,"
+    },
+    {
+        "name": "Catering",
+        "class": "three d",
+        "sub": "manage",
+        "years": "8",
+        "dateend": "1403169742000",
+        "video": "true",
+        "rec": "9",
+        "high_salary": "45",
+        "tasks": "servers,\nbuffet line,\nequipment rental ,\nseating"
+    },
+    {
+        "name": "Catering",
+        "class": "three e",
+        "sub": "cert",
+        "years": "8",
+        "dateend": "1403169742000",
+        "video": "true",
+        "rec": "3",
+        "high_salary": "45",
+        "tasks": "ABC Manager,\nFood Handling Certification,\nFood Safety Proctor"
+    },
+    {
+        "name": "other",
+        "class": "four a",
+        "sub": "service",
+        "years": "6",
+        "dateend": "1403169742000",
+        "video": "true",
+        "rec": "0",
+        "high_salary": "0",
+        "tasks": "cold dish, \nhot dish,\nbaker,\noven,\nfryer,\nsauteeing"
+    },
+    {
+        "name": "other",
+        "class": "four b",
+        "sub": "prep",
+        "years": "4",
+        "dateend": "1403169742000",
+        "video": "true",
+        "rec": "0",
+        "high_salary": "0",
+        "tasks": ""
+    },
+    {
+        "name": "other",
+        "class": "four c",
+        "sub": "maintenance",
+        "years": "2",
+        "dateend": "1403169742000",
+        "video": "false",
+        "rec": "0",
+        "high_salary": "0",
+        "tasks": ""
+    },
+    {
+        "name": "other",
+        "class": "four d",
+        "sub": "manage",
+        "years": "3",
+        "dateend": "1403169742000",
+        "video": "false",
+        "rec": "0",
+        "high_salary": "0",
+        "tasks": "menu planning"
+    },
+    {
+        "name": "other",
+        "class": "four e",
+        "sub": "cert",
+        "years": "4",
+        "dateend": "1403169742000",
+        "video": "false",
+        "rec": "1",
+        "high_salary": "0",
+        "tasks": "CPR"
+    }
+]
+			}
+
+
+	 ]);
